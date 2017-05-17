4ca0800a73cb529d9e323389f5f129a3b4007068
胡遵榕 <intern_huzunrong@cvte.com>
2017-05-17 18:30:02 +0800

    优化

---
+++ test/cvter/intern/service/UserServiceTest.java
@@ -0,0 +1,59 @@
+package cvter.intern.service;
+
+import cvter.intern.model.User;
+import cvter.intern.utils.UIDUtil;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * Created by cvter on 2017/5/16.
+ */
+//整合junit和spring，让junit在启动时候加载springIOC容器
+@RunWith(SpringJUnit4ClassRunner.class)
+//告诉junit spring的配置文件,需要用到的配置文件，如果是dao的话可以不用spring-mvc.xml
+@ContextConfiguration({/*"classpath:spring-mvc.xml",*/ "classpath:spring-mybatis.xml"})
+public class UserServiceTest {
+
+    @Resource /*或者 @Autowired */
+    private UserService userService;
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Test
+    public void getByUid() throws Exception {
+    }
+
+    @Test
+    public void getPaginate() throws Exception {
+        List<User> userList = userService.getPaginate(1, 5);
+        for (User user :
+                userList) {
+            System.out.println(user);
+        }
+        System.out.println(UIDUtil.getRandomUID());
+    }
+
+    @Test
+    public void save() throws Exception {
+    }
+
+    @Test
+    public void update() throws Exception {
+    }
+
+    @Test
+    public void delete() throws Exception {
+    }
+
+    @Test
+    public void deleteByUid() throws Exception {
+    }
+
+}
