4fb4091669221f6ff97da299eb24eb6f17a38793
wz <1017834531@qq.com>
2017-05-18 16:04:33 +0800

    规范代码

--- src/main/java/cvter/intern/dao/BookInfoMapper.java
+++ src/main/java/cvter/intern/dao/BookInfoMapper.java
@@ -1,37 +1,27 @@
 package cvter.intern.dao;
 
 import cvter.intern.model.BookInfo;
-import cvter.intern.model.BookInfoExample;
-import java.util.List;
-import org.apache.ibatis.annotations.Param;
 
 public interface BookInfoMapper {
-//    int countByExample(BookInfoExample example);
 
-//    int deleteByExample(BookInfoExample example);
-
-    int deleteByPrimaryKey(String uid);
-
+    /**
+     * 增加记录
+     */
     int insert(BookInfo record);
 
-//    int insertSelective(BookInfo record);
+    /**
+     * 删除记录
+     */
+    int deleteByPrimaryKey(String uid);
 
-//    List<BookInfo> selectByExampleWithBLOBs(BookInfoExample example);
+    /**
+     * 更新记录
+     */
+    int updateByPrimaryKey(BookInfo record);
 
-//    List<BookInfo> selectByExample(BookInfoExample example);
-
-    //通过Uid查询
+    /**
+     * 查询记录
+     */
     BookInfo selectByPrimaryKey(String uid);
 
-    int updateByExampleSelective(@Param("record") BookInfo record, @Param("example") BookInfoExample example);
-
-    int updateByExampleWithBLOBs(@Param("record") BookInfo record, @Param("example") BookInfoExample example);
-
-    int updateByExample(@Param("record") BookInfo record, @Param("example") BookInfoExample example);
-
-    int updateByPrimaryKeySelective(BookInfo record);
-
-    int updateByPrimaryKeyWithBLOBs(BookInfo record);
-
-    int updateByPrimaryKey(BookInfo record);
 }
