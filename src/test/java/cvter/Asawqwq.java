f91a7a3b2e1e9e1afb23fbdbfa28bf33004268e1
mapei <mapei@cvte.com>
2017-05-23 15:07:58 +0800

    Redis函数封装

--- src/test/java/cvter/Asawqwq.java
+++ src/test/java/cvter/Asawqwq.java
@@ -2,9 +2,7 @@ package cvter;
 
 import cvter.intern.dao.PanicDao;
 import cvter.intern.dao.RoleDao;
-import cvter.intern.model.Panic;
-import cvter.intern.model.Role;
-import cvter.intern.model.RoleInfoExample;
+import cvter.intern.redis.service.RedisService;
 import cvter.intern.service.DiscountService;
 import cvter.intern.service.PanicService;
 import cvter.intern.service.RoleService;
@@ -13,13 +11,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.web.servlet.MvcResult;
-
-import javax.annotation.Resource;
-import java.util.Date;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Created by cvter on 2017/5/16.
@@ -43,34 +36,37 @@ public class Asawqwq{
     @Autowired
     private DiscountService discountService;
 
+    @Autowired
+    private RedisService redisService;
+
    // private RoleInfo roleInfo;
     
     //@Autowired
     //private RoleService roleService;
-
-    @Test
-    public void bookDel(){
-
+   @Test
+    public void testvset(){
+       redisService.vset("mp","mapei");
+       //redisService.deleteKey();
     }
-    @Test
-    public void testSelectRole(){
-//        RoleInfoExample example = new RoleInfoExample();
-//        example.createCriteria().andIdEqualTo(2);
-       // roleInfo.setUid("11111");
-       // roleInfoMapper.insert(roleInfo);
-       //roleInfoMapper.selectByExample(example);
-
-        roleInfoMapper.selectByPrimaryKey("mm");
-        //roleInfoMapper.deleteByPrimaryKey("mm");
-      //  roleInfoMapper.insert(new RoleInfo(1,"nn","dd",false,new Date(2015),new Date(2015)));
-        //roleInfoMapper.updateByPrimaryKey(new RoleInfo(1,"mm","dd",false,new Date(2015),new Date(2015)));
-
-//        panicInfoMapper.selectByPrimaryKey("mm");
-//        panicInfoMapper.deleteByPrimaryKey("mm");
-//        panicInfoMapper.insert(new PanicInfo(1,"mm",(byte)2,6,new Date(2015),new Date(2015),false,new Date(2015),new Date(2015)));
-//        panicService.deleteByUid("mm");
-
-        //discountService.selectByUID("mm");
-
-    }
+ //   @Test
+//    public void testSelectRole(){
+////        RoleInfoExample example = new RoleInfoExample();
+////        example.createCriteria().andIdEqualTo(2);
+//         //roleInfo.setUid("11111");
+//       // roleInfoMapper.insert(roleInfo);
+//       //roleInfoMapper.selectByExample(example);
+//
+//       roleInfoMapper.selectByPrimaryKey("mm");
+//        //roleInfoMapper.deleteByPrimaryKey("mm");
+//      //  roleInfoMapper.insert(new RoleInfo(1,"nn","dd",false,new Date(2015),new Date(2015)));
+//        //roleInfoMapper.updateByPrimaryKey(new RoleInfo(1,"mm","dd",false,new Date(2015),new Date(2015)));
+//
+////        panicInfoMapper.selectByPrimaryKey("mm");
+////        panicInfoMapper.deleteByPrimaryKey("mm");
+////        panicInfoMapper.insert(new PanicInfo(1,"mm",(byte)2,6,new Date(2015),new Date(2015),false,new Date(2015),new Date(2015)));
+////        panicService.deleteByUid("mm");
+//
+//        //discountService.selectByUID("mm");
+//
+//    }
 }
--- src/test/java/cvter/intern/service/RedisTest.java
+++ src/test/java/cvter/intern/service/RedisTest.java
@@ -1,6 +1,7 @@
 package cvter.intern.service;
 
 import cvter.intern.model.Book;
+import cvter.intern.redis.service.RedisService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,11 +15,14 @@ import java.util.UUID;
  * Created by cvter on 2017/5/23.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "classpath:spring-redis.xml")
+@ContextConfiguration(locations = {"classpath:spring-redis.xml","classpath:spring-mybatis.xml"})
 public class RedisTest {
 
     @Autowired
     private RedisTemplate<String,Book> redisTemplate;
+
+    @Autowired
+    private RedisService redisService;
 
     @Test
     public void testSet(){
@@ -32,5 +36,29 @@ public class RedisTest {
     @Test
     public void testGet(){
         System.out.println(redisTemplate.boundValueOps("mapei").get());
+    }
+
+//    @Test
+//    public void testRedis(){
+//        redisService.deleteKey("mapei");
+//    }
+
+    @Test
+    public void testvset(){
+//        redisService.vset("mp","mapei");
+//        System.out.println(redisService.vset("mp","mama"));
+//        System.out.println(redisService.vget("mp"));
+//        redisService.deleteKey("mp");
+//        System.out.println(redisService.vget("mp"));
+//        redisService.vput("wz","wangzhe");
+//        redisService.vput("wz","wangwang");
+//        System.out.println(redisService.vget("wz"));
+        redisService.vset("mp","mapei");
+        System.out.println(redisService.vget("mp"));
+        System.out.println(redisService.vset("mp","mama"));
+        System.out.println(redisService.vget("mp"));
+        redisService.deleteKey("mp");
+        System.out.println(redisService.vget("mp"));
+        //redisService.deleteKey();
     }
 }
